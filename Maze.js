37d1544281d80746e6b4d3940ac785cf7290ba93
LangesComputerScience <langescomputerscience@gmail.com>
2025-01-24 07:59:05 -0500

    Init Maze.js Program

---
+++ Maze.js
@@ -0,0 +1,81 @@
+//Team Members: Mr. Lange & Mr. Payton
+//Date: 1.24.2025
+//Program: Maze
+//Hardware: Bolt #SB-6B78
+
+//Begin Program
+async function startProgram() {
+
+//Start & Say Start
+//Bolt will say Start across the LED's & Say it out loud
+//Driver: Mr. Payton
+//Navigator: Mr. Lange
+
+
+
+
+//Green Checkpoint
+//LED's turn Green
+//Driver: Mr. Lange
+//Navigator: Mr. Payton
+
+
+
+
+//Sound #1 Checkpoint
+//Bolt makes sound of choice - Update Sound
+//Driver: Mr. Payton
+//Navigator: Mr. Lange
+
+
+
+
+//Red Checkpoint
+//LED's turn Red
+//Driver: Mr. Lange
+//Navigator: Mr. Payton
+
+
+
+
+//Sound #2 Checkpoint
+//Bolt makes sound of choice - Update Sound
+//Driver: Mr. Payton
+//Navigator: Mr. Lange
+
+
+
+
+//Purple Checkpoint
+//LED's turn Purple
+//Driver: Mr. Lange
+//Navigator: Mr. Payton
+
+
+
+
+//Sound #3 Checkpoint
+//Bolt makes sound of choice - Update Sound
+//Driver: Mr. Payton
+//Navigator: Mr. Lange
+
+
+
+
+//Blue Checkpoint
+//LED's turn Purple
+//Driver: Mr. Lange
+//Navigator: Mr. Payton
+
+
+
+//Finished & Say Finished
+//Bolt will say Finished across the LED's & Say it out loud
+//Driver: Mr. Payton
+//Navigator: Mr. Lange
+
+
+
+
+//End Program
+}
